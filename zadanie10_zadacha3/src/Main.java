055b03bfdd0eb973cc2a47d1b23b1fc4dc674aba
madasmr <167167835+madasmr@users.noreply.github.com>
2025-01-10 22:22:44 +0700

    Main.java

--- zadanie10_zadacha3/src/Main.java
+++ zadanie10_zadacha3/src/Main.java
@@ -1,10 +1,8 @@
 import java.util.Arrays;
 import java.util.Random;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
             Random random = new Random();
             int[] array = new int[10];
             int maximum;
